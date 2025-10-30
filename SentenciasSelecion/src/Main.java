d73cefedabd097f288c9bd149c532f7eaf6b6664
Senra04 <senra04@gmail.com>
2025-10-30 19:44:45 +0100

    ok
    desde pc

--- SentenciasSelecion/src/Main.java
+++ SentenciasSelecion/src/Main.java
@@ -2,6 +2,12 @@ public class Main {
     public static void main(String[] args) {
         SentenciaIF sentenciaIF = new SentenciaIF();
         // sentenciaIF.examenAprobado(9);
-        sentenciaIF.notaExamen(10);
+        // sentenciaIF.notaExamen(10);
+        SentenciaSwitch sentenciaSwitch = new SentenciaSwitch();
+        //sentenciaSwitch.usoSwitchInt();
+        //sentenciaSwitch.usoSwitchString();
+        //sentenciaSwitch.usoSwitchChar();
+        //sentenciaSwitch.usoSwitchLambda();
+        sentenciaSwitch.menuOpciones();
     }
 }
